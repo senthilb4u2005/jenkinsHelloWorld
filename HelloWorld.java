344c52f4bd53c027368131711354ea27fc30e42d
Senthilkumar <senthilb4u2005@gmail.com>
2020-05-09 14:33:26 +0530

    Added first commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
 	public static void main(String args[]){
-		System.out.println("Hello world!... My first java programe with Jenking build server");
+		System.out.println("First push throught git");
 	}
 }
